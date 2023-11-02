24bbc708fb51d6039d2e58bbfc056f409cc2ea5e
olehandro <odpash.itmo@gmail.com>
2023-11-02 13:25:35 +0300

    test v. 0.0.0

--- app/working_modes/knowledge_mode.py
+++ app/working_modes/knowledge_mode.py
@@ -61,7 +61,7 @@ class KnowledgeMode:
         print(func)
         try:
             messages = [
-                {'role': 'system', 'content': 'Give answer:'},
+                {'role': 'system', 'content': 'Please classificate:'},
                 {"role": "user",
                  "content": message}]
             openai.api_key = openai_api_key
@@ -75,7 +75,7 @@ class KnowledgeMode:
         except Exception as e:
             print(e)
             return {'is_ok': False, 'args': {}}
-        print(response_message['content'])
+        print(response_message)
         if response_message.get("function_call"):
             function_args = json.loads(response_message["function_call"]["arguments"])
             return {'is_ok': True, 'args': function_args}
