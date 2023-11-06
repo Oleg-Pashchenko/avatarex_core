b612e2c58821e60eb7fb4488f872362aed8b2fd4
olehandro <odpash.itmo@gmail.com>
2023-11-06 14:19:51 +0300

    test v. 0.0.0x

--- app/working_modes/knowledge_mode.py
+++ app/working_modes/knowledge_mode.py
@@ -106,7 +106,7 @@ class KnowledgeMode:
             return perephrase(bounded_situations.database_error_message, openai_api_key)
         print("Квалифицирован вопрос:", response['args'])
         print('Получен ответ из базы данных:', answer)
-        response = perephrase(answer, openai_api_key, descr='сформулируй ответ воедино без повторений фактов при этом ничего нового не добавляя.')
+        response = perephrase(answer, openai_api_key, descr='сформулируй ответ воедино без повторений фактов при этом ничего нового не добавляя')
         print('Перефразирован ответ:', response)
         return response
 
