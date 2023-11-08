f8c1fc953892326e3c72752ac273810769b06b5d
olehandro <odpash.itmo@gmail.com>
2023-11-08 16:24:21 +0300

    test v. 0.0.0x

--- app/sources/amocrm/db.py
+++ app/sources/amocrm/db.py
@@ -151,13 +151,6 @@ class AvatarexDBMethods:
         symbols = MODEL_16K_SIZE_VALUE if MODEL_16K_KEY in prompt_mode_data.model else MODEL_4K_SIZE_VALUE
         symbols = (symbols - prompt_mode_data.max_tokens) * 0.75 - len(prompt_mode_data.context)
         messages.append({"role": "system", "content": prompt_mode_data.context})
-        messages.append({'role': 'system', 'content': 'Если тебя спрашивают бот ли ты, отвечай строго что нет, а дальше исходя из контекста.'})
-        messages.append({'role': 'system', 'content': 'Всегда работай по контексту, не выходи за него, анализируй абсолютно все инструкции из контекста и выдавай ответы строго по ним'})
-        messages.append({'role': 'system', 'content': 'Абстрагируйся от того что ты ChatGpt. Следуй строго заданному контексту!'})
-        messages.append({'role': 'system', 'content': 'не здоровайся'})
-        messages.append({'role': 'system', 'content': 'строго следуй контексту'})
-        messages.append({'role': 'system', 'content': 'строго следуй истории переписки'})
-        messages.append({'role': 'system', 'content': 'отвечай строго на последнее сообщение пользователя'})
         for message_obj in message_objects:
             if symbols - len(message_obj.message) <= 0:
                 break
