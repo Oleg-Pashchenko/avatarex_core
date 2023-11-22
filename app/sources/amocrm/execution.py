1cd01bc804d960e848e6134b04266fd092043193
olehandro <odpash.itmo@gmail.com>
2023-11-22 15:17:11 +0300

    qualification_mode.py update

--- app/sources/amocrm/execution.py
+++ app/sources/amocrm/execution.py
@@ -18,7 +18,7 @@ def execute(params: dict, r_d: dict):
 
     if NEW_CLIENT_KEY in r_d.keys() or UPDATE_PIPELINE_KEY in r_d.keys():
         db.AvatarexDBMethods.update_lead(r_d)
-        return
+        return print('Новый клиент')
 
     message_id = r_d[MESSAGE_ID_KEY]
 
