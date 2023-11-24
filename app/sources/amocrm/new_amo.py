2db7c6d429cf5e2f91150984012b7662c887e9f1
olehandro <odpash.itmo@gmail.com>
2023-11-25 00:32:33 +0300

    qualification_mode.py update

--- app/sources/amocrm/new_amo.py
+++ app/sources/amocrm/new_amo.py
@@ -82,8 +82,9 @@ class AmoConnect:
 
     def get_second_last_message(self, lead_id):
         url = f'{self.host}/ajax/v3/leads/{lead_id}/events_timeline'
-        response = self.session.get(url)
-        print(response.text)
+        response = self.session.get(url).json()['_embedded']['items']
+        for i in response:
+            print(i['message']['text'], i['author']['origin'])
 
     def get_params_information(self, fields: list):
         result = {}
